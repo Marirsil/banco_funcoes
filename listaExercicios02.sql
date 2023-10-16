8927354cb9994f4551419926b4669be450e91fb9
Marirsil <110177157+Marirsil@users.noreply.github.com>
2023-10-15 22:39:10 -0300

    Funções Numéricas

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -21,3 +21,20 @@ SELECT
         ELSE CONCAT('Sr. ', nome)
     END AS nome_com_tratamento
 FROM nomes;
+
+CREATE TABLE produtos (
+    produto VARCHAR(50),
+    preco DECIMAL(10, 2),
+    quantidade INT
+);
+
+INSERT INTO produtos (produto, preco, quantidade) VALUES
+    ('Produto A', 19.99, 5),
+    ('Produto B', 25.50, 8),
+    ('Produto C', 9.99, 0);
+
+SELECT produto, ROUND(preco, 2) AS preco_arredondado FROM produtos;
+
+SELECT produto, ABS(quantidade) AS quantidade_absoluta FROM produtos;
+
+SELECT AVG(preco) AS media_precos FROM produtos;
