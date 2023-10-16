5d39f5400fe26be21e0e2f04c000fbc67c8a8c22
Marirsil <110177157+Marirsil@users.noreply.github.com>
2023-10-15 22:39:53 -0300

    Funções de Data

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -38,3 +38,21 @@ SELECT produto, ROUND(preco, 2) AS preco_arredondado FROM produtos;
 SELECT produto, ABS(quantidade) AS quantidade_absoluta FROM produtos;
 
 SELECT AVG(preco) AS media_precos FROM produtos;
+
+CREATE TABLE eventos (
+    data_evento DATE
+);
+
+INSERT INTO eventos (data_evento) VALUES
+    ('2023-10-15'),
+    ('2023-10-20'),
+    ('2023-11-05');
+
+INSERT INTO eventos (data_evento) VALUES (NOW());
+
+SELECT
+    DATEDIFF('2023-11-05', '2023-10-15') AS dias_entre_datas
+FROM eventos
+WHERE data_evento = '2023-11-05';
+
+SELECT data_evento, DAYNAME(data_evento) AS dia_da_semana FROM eventos;
