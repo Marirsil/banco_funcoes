54c221bc8d6b1e88bd9d327ffad333089bc9c7c1
Marirsil <110177157+Marirsil@users.noreply.github.com>
2023-10-15 22:47:15 -0300

    Função Personalizada

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -70,3 +70,17 @@ SELECT
         ELSE 'Caro'
     END AS categoria_preco
 FROM produtos;
+
+DELIMITER //
+CREATE FUNCTION TOTAL_VALOR(preco DECIMAL(10, 2), quantidade INT)
+RETURNS DECIMAL(10, 2)
+BEGIN
+    DECLARE total DECIMAL(10, 2);
+    SET total = preco * quantidade;
+    RETURN total;
+END;
+//
+DELIMITER ;
+
+SELECT produto, quantidade, preco, TOTAL_VALOR(preco, quantidade) AS valor_total
+FROM produtos;
