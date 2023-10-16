8f51b6ac17364e5867d1cd12375392b56230e372
Marirsil <110177157+Marirsil@users.noreply.github.com>
2023-10-15 22:30:10 -0300

    tabela nomes com uma coluna

---
+++ listaExercicios02.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE listaFuncoes;
+USE listaFuncoes;
+
+CREATE TABLE nomes (
+    nome VARCHAR(50)
+);
+
+INSERT INTO nomes (nome) VALUES
+    ('Roberta'),
+    ('Roberto'),
+    ('Maria Clara'),
+    ('João');
